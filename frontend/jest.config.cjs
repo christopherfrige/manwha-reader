45042e4d1e2188495cfa6bfb88a8e94884ff2e0d
christopherfrige <chrisfrige@gmail.com>
2024-03-13 18:22:59 -0300

    feat: tests and linters run adjustments

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -9,8 +9,6 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  coveragePathIgnorePatterns: ['/node_modules/', '/tests/'],
-  coverageReporters: ['text', 'json-summary'],
   // Fix in order for vue-test-utils to work with Jest 29
   // https://test-utils.vuejs.org/migration/#test-runners-upgrade-notes
   testEnvironmentOptions: {
